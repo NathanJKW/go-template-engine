1781c80cc78e50cc840d99562de3bc6233bf34fe
NathanJKW <NathanJKWilson@outlook.com>
2024-01-13 11:22:12 +0000

    Added flag for action

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -1,1 +1,17 @@
 package main
+
+import (
+	"flag"
+	"fmt"
+	"os"
+)
+
+func main() {
+	actionName := flag.String("action", "", "Action to perform.")
+	flag.Parse()
+
+	if *actionName == "" {
+		fmt.Println("No Action Specified")
+		os.Exit(1)
+	}
+}
