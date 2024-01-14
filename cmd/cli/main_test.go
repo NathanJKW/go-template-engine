290d4baa3f1ffec93f71caef77820c84284ea6a6
NathanJKW <NathanJKWilson@outlook.com>
2024-01-14 03:13:16 +0000

    Implemented better test structure

--- cmd/cli/main_test.go
+++ cmd/cli/main_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"errors"
 	"os"
 	"testing"
 )
@@ -10,28 +11,69 @@ func TestFlagParsing(t *testing.T) {
 	originalArgs := os.Args
 	defer func() { os.Args = originalArgs }()
 
-	// Set os.Args with test data
-	os.Args = []string{
-		"gte",
-		"new",
-		"-t", "Template Name",
-		"-o", "./this/new/path"}
-
-	// Parse the args passed in when running
-	templateName, outputPath, err := ParseArgs()
-
-	// Test - Error return this should never happen
-	// as this is a happy path test.
-	if err != nil {
-		t.Errorf("This shouldnt happen %s", err)
+	tests := []struct { // test case defintion
+		name             string
+		args             []string
+		wantTemplateName string
+		wantPathName     string
+		wantError        error
+	}{ // test Cases
+		{ // test case Happy
+			name: "Perfect case",
+			args: []string{
+				"gte",
+				"new",
+				"-t", "Template Name",
+				"-o", "./this/new/path"},
+			wantTemplateName: "Template Name",
+			wantPathName:     "./this/new/path",
+			wantError:        nil,
+		},
+		{ // test case Happy
+			name: "No output specified",
+			args: []string{
+				"gte",
+				"new",
+				"-t", "Template Name"},
+			wantTemplateName: "Template Name",
+			wantPathName:     "./",
+			wantError:        nil,
+		},
+		{ // test case no template
+			name: "no template specified",
+			args: []string{
+				"gte",
+				"new"},
+			wantTemplateName: "",
+			wantPathName:     "",
+			wantError:        errors.New("no template specified"),
+		},
 	}
 
-	
-	if templateName != "Template Name" {
-		t.Errorf("Wrong TemplateName returned %s", templateName)
-	}
-
-	if outputPath != "./this/new/path" {
-		t.Errorf("Wrong TemplateName returned %s", outputPath)
+	// Execute each test case
+	for _, tt := range tests { // test case executions
+		t.Run(tt.name, func(t *testing.T) {
+			// set os.Args
+			os.Args = tt.args
+			// function call
+			gotTemplateName, gotPathName, gotError := newArgsHandler()
+			// assert
+			if gotTemplateName != tt.wantTemplateName {
+				t.Errorf("ParseArgs TemplateName got %s; want %s",
+					tt.wantTemplateName, gotTemplateName)
+			}
+			// assert
+			if gotPathName != tt.wantPathName {
+				t.Errorf("ParseArgs TemplateName got %s; want %s",
+					tt.wantPathName, gotPathName)
+			}
+			// assert
+			if gotError != nil {
+				if errors.Is(tt.wantError, gotError) {
+					t.Errorf("ParseArgs TemplateName got %s; want %s",
+						tt.wantError, gotError)
+				}
+			}
+		})
 	}
 }
