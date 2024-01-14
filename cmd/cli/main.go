191ecf6d1b154dcab7385a53760628e3f6d4108d
NathanJKW <NathanJKWilson@outlook.com>
2024-01-14 03:14:49 +0000

    Added switch to catch commands can now have different flags for different commands

--- cmd/cli/main.go
+++ cmd/cli/main.go
@@ -10,29 +10,51 @@ func main() {
 	ParseArgs()
 }
 
-func ParseArgs() (string, string, error) {
+func ParseArgs() {
+	switch os.Args[1] {
+	case "new":
+		// gotTemplateName, gotPathName, err := newArgsHandler()
 
-	if os.Args[1] == "new" {
-		originalArgs := os.Args
-		defer func() { os.Args = originalArgs }()
+		// if err != nil {
+		// 	fmt.Printf("Error while trying to parse args %s", err)
+		// }
 
-		os.Args = RemoveIndex(os.Args, 1)
-
-		templateName := flag.String("t", "", "Template Name")
-		outputPath := flag.String("o", "", "Template Name")
-		flag.Parse()
-
-		// Validate Template
-		if *templateName == "" {
-			return "", "", errors.New("no template specified")
-		}
-
-		return *templateName, *outputPath, nil
+	default:
+		//return "", "", errors.New("command not found")
 	}
-
-	return "", "", errors.New("command not found")
 }
 
+func newArgsHandler() (string, string, error) {
+	// back up os.Args
+	originalArgs := os.Args
+	// restore the origonal os.Args after function is finished
+	defer func() { os.Args = originalArgs }()
+
+	// get new flag set
+	// this is to stop flag redified error when testing
+	flag.CommandLine = flag.NewFlagSet(os.Args[0], flag.ExitOnError)
+
+	// cant specify where to start looking
+	// for flags so you have to remove index 1
+	// as im using index 1 to specify a command
+	os.Args = RemoveIndex(os.Args, 1)
+	// bind flags
+	templateName := flag.String("t", "", "Template Name")
+	outputPath := flag.String("o", "./", "Output path")
+	// parse flags
+	flag.Parse()
+
+	// make sure a template has been specified
+	if *templateName == "" {
+		return "", "", errors.New("no template specified")
+	}
+
+	// return template name and output path
+	return *templateName, *outputPath, nil
+}
+
+// Function creates a new slice and copies old slice
+// removes the specified index and returns the new slice
 func RemoveIndex(s []string, index int) []string {
 	ret := make([]string, 0)
 	ret = append(ret, s[:index]...)
